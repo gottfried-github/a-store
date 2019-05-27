1e6d748735dba94bae560d36f858b53365c78b5f
wsa <dannvx@gmail.com>
2019-05-27 03:54:55 +0300

    add dist files

---
+++ dist/js/getViewportDims/getViewport.js
@@ -0,0 +1,28 @@
+/*
+TODO: export this fn;
+*/
+// https://stackoverflow.com/questions/6942785/window-innerwidth-vs-document-documentelement-clientwidth
+// https://bugzilla.mozilla.org/show_bug.cgi?id=156388#c14
+function getViewportHeight() {
+  // getElementsByTagName, if I'm not mistaken returns a livelist (hell knows what that is, but it's
+  // updated live - as dom gets changed). I'm not sure about using it, it behaved misteriously once...
+  // But, querySelector is not so compatible.
+  // Maybe: (document.querySelector('body') || document.getElementsByTagName('body')[0].clientWidth)
+  return window.innerHeight && document.documentElement.clientHeight ?
+    Math.min(window.innerHeight, document.documentElement.clientHeight) :
+    window.innerHeight || document.documentElement.clientHeight
+      || (document.querySelector('body') || document.getElementsByTagName('body')[0].clientHeight);
+}
+
+function getViewportWidth() {
+  // getElementsByTagName, if I'm not mistaken returns a livelist (hell knows what that is, but it's
+  // updated live - as dom gets changed). I'm not sure about using it, it behaved misteriously once...
+  // But, querySelector is not so compatible.
+  // Maybe: (document.querySelector('body') || document.getElementsByTagName('body')[0].clientWidth)
+  return window.innerWidth && document.documentElement.clientWidth ?
+    Math.min(window.innerWidth, document.documentElement.clientWidth) :
+    window.innerWidth || document.documentElement.clientWidth
+      || (document.querySelector('body') || document.getElementsByTagName('body')[0].clientWidth);
+}
+
+export {getViewportWidth, getViewportHeight}
